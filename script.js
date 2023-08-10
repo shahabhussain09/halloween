e5cafb6e7a07326aef06aff9bbd679578074b080
shahabhussain09 <shahabkhan0981233@gmail.com>
2023-08-10 18:14:16 +0500

    Halloween

---
+++ script.js
@@ -0,0 +1,62 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active')
+    }else{
+        document.querySelector('.header').classList.remove('active')
+    }
+}
+
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+  });
+  
+  var swiper = new Swiper(".product-slider", {
+    spaceBetween: 20,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    grabCursor:true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+    },
+  });
